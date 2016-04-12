52322858acd83e385d112a97a2a6dab92e2440f8
ajxm <anjingxiaomo@gmail.com>
2016-04-12 14:30:50 +0800

    Initial commit

---
+++ src/java/com/javen/service/IUserService.java
@@ -0,0 +1,10 @@
+package com.javen.service;
+
+import com.javen.model.User;
+
+/**
+ * Created by AJXM on 2016/4/11.
+ */
+public interface IUserService {
+    public User getUserById(int userId);
+}
